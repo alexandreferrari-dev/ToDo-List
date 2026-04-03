86146ca24bcc3c223ab0a4e8f5c91971f0dc97e1
Alexandre Ferrari <ale.vertigem@gmail.com>
2026-04-03 11:14:49 -0300

    criando-componente-text-com-cva

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Text from "./components/text.tsx";
+
 export default function App() {
-  return <div className="text-red-500">Olá mundo!!</div>
-   
+  return (
+  <div className="flex flex-col gap-2">
+  <Text variant="body-sm-bold" className='text-pink-base'>
+    Olá mundo!!
+  </Text>
+  <Text className='text-green-base'>
+    Olá mundo!!
+  </Text>
+  <Text variant="body-md-bold" className='text-pink-base'>
+    Olá mundo!!
+  </Text>
+  <Text>Levar o dog para passear</Text>
+  </div>
+  )
 }
 
